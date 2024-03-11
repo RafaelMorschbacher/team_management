1dfe59e62d4605fea02183dbcd715ba4893e0017
RafaelMorschbacher <rafa2002cunha@gmail.com>
2024-03-11 19:21:35 -0300

    allocation model and controller for creation, deletion and listing

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response, application } from 'express'
 import { employeeRoutes } from './routes/employeeRoutes';
 import {projectRoutes} from './routes/projectRoutes'
 import body_parser from 'body-parser'
+import { allocation_routes } from './routes/allocationRoutes';
 
 const app:Application = express();
 
@@ -11,6 +12,7 @@ app.use(body_parser.json())
 //REQUESTS
 app.use('/employee', employeeRoutes)
 app.use('/project', projectRoutes)
+app.use('/allocation', allocation_routes)
 
 //listen for requests
 const port = process.env.PORT || 3000;
