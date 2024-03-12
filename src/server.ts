71fe5324dda209176c1c5984495e6d1ae38fa53d
RafaelMorschbacher <rafa2002cunha@gmail.com>
2024-03-12 19:36:07 -0300

    swagger-ui setup for documentation

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,24 @@
 import express, { Application, Request, Response, application } from 'express'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocs from './swagger.json'
 import { employeeRoutes } from './routes/employeeRoutes';
 import {projectRoutes} from './routes/projectRoutes'
 import body_parser from 'body-parser'
 import { allocation_routes } from './routes/allocationRoutes';
+
 
 const app:Application = express();
 
 //CONFIGS
 app.use(body_parser.json())
 
-//REQUESTS
+//Routes
 app.use('/employee', employeeRoutes)
 app.use('/project', projectRoutes)
 app.use('/allocation', allocation_routes)
+
+// Documentation with swaggrer
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 //listen for requests
 const port = process.env.PORT || 3000;
